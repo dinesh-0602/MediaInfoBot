3c227e0fda1afeac42e2280f65910b2a4f1401b5
SUPERMAN <55796045+AswanthVK@users.noreply.github.com>
2021-08-09 11:09:24 +0530

    Update common.py

--- mega/telegram/plugins/common.py
+++ mega/telegram/plugins/common.py
@@ -1,40 +1,15 @@
 from pyrogram import filters, emoji, Client
 from pyrogram.types import Message
-from mega.database.files import MegaFiles
-from mega.database.users import MegaUsers
+#from mega.database.files import MegaFiles
+#from mega.database.users import MegaUsers
 from ...telegram import Common
 
 
 @Client.on_message(filters.command("start", prefixes=["/"]))
 async def start_message_handler(c: Client, m: Message):
-    await MegaUsers().insert_user(m.from_user.id)
-    if len(m.command) > 1:
-        if m.command[1].split("-")[0] == 'plf':
-            file_id = m.command[1].split("-", 1)[1]
-            file_details = await MegaFiles().get_file_by_file_id(file_id)
-
-            if file_details is not None:
-                file_message = await c.get_messages(
-                    chat_id=file_details['chat_id'],
-                    message_ids=file_details['msg_id']
-                )
-
-                if str(file_details['file_type'].split("/"))[0].lower() == "video":
-                    await m.reply_video(
-                        video=file_message.video.file_id
-                    )
-                elif str(file_details['file_type'].split("/"))[0].lower() == "audio":
-                    await m.reply_audio(
-                        audio=file_message.audio.file_id
-                    )
-                else:
-                    await m.reply_document(
-                        document=file_message.document.file_id
-                    )
-    else:
-        await m.reply_text(
-            text=f"Hello! My name is Megatron {emoji.MAN_BOWING_DARK_SKIN_TONE}"
-        )
+    await m.reply_text(
+        text=f"Hello! My name is Megatron {emoji.MAN_BOWING_DARK_SKIN_TONE}"
+    )
 
 
 @Client.on_message(group=-1)
